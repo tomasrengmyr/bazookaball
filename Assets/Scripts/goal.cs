d6a17894db0a38d4b5d700acf2c7ac85f754b038
peder <peder.a.jonsson@gmail.com>
2017-04-08 12:41:27 +0200

    added a goal with hit detection on ball

---
+++ Assets/Scripts/goal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Ball")) {
+			Debug.Log ("GOAL");
+			other.gameObject.SetActive (false);
+
+		} else {
+			Debug.Log ("GOAL tag was not ball");
+		}
+	}
+
+}
