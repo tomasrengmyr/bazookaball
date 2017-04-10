aedac570a56bcd279e1308fcc1a1ac72dd6bd780
Jon Andersson <andersson.jon@gmail.com>
2017-04-10 20:29:20 +0200

    Readding from commit c2c270d3c5794a4b345feb840c4509f6309baf0f. Because github did stuff I did want, fucking never make commits from github.

--- Assets/Scripts/BulletHitDitection.cs
+++ Assets/Scripts/BulletHitDitection.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletHitDitection : MonoBehaviour {
+	private Vector3 lastPosition;
+	private Vector3 newPosition;
+	private Vector3 distance;
 
-	//Explosion properties
+	//Explosion
 	public float radius = 5.0F;
 	public float power = 10.0F;
 	// Use this for initialization
@@ -28,11 +31,17 @@ public class BulletHitDitection : MonoBehaviour {
 
 	void OnCollisionEnter(Collision collision)
 	{
+		//Debug.Log ("collision ID: " + collision.gameObject.name);
 		foreach (ContactPoint contact in collision.contacts)
 		{
 			Debug.DrawRay(contact.point, contact.normal, Color.white);
 		}
 		if (collision.relativeVelocity.magnitude > 2) {
+			//Debug.Log ("POWER in bullet!  " + characterController.instance.GetLoadPower());
+			//talk to global game manager
+			//MainGameManager.instance.AdjustScore (1);
+
+
 			Vector3 explosionPos = transform.position;
 			Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
 			foreach (Collider hit in colliders)
@@ -44,13 +53,16 @@ public class BulletHitDitection : MonoBehaviour {
 			}
 			bulletRigidBody.isKinematic = true;
 			Explode();
+			//Destroy(gameObject);
 		}
+			
 	}
 
 	void Explode() {
+		
 		var exp = GetComponent<ParticleSystem>();
 		exp.Play();
 		characterController.instance.SetLoadPower (0);
-		Destroy(gameObject, exp.main.duration);
+		Destroy(gameObject, exp.duration);
 	}
 }
