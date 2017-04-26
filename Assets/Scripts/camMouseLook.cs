f3b5d4fcefe100c975ca5de0ced7d020b29960ea
peder <peder.a.jonsson@gmail.com>
2017-04-26 19:05:25 +0200

    locked to 180

--- Assets/Scripts/camMouseLook.cs
+++ Assets/Scripts/camMouseLook.cs
@@ -25,24 +25,21 @@ public class camMouseLook : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// OBS pseduo code have no controller to test with.
-		//Debug.Log(character.name);
-		/*var md = this.CompareTag (PLAYER_TWO) ? 
-			new Vector2 (Input.GetAxisRaw ("Joystick X"), Input.GetAxisRaw ("Joystick Y")) : 
-			new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
-		*/
 
 		var md = character.name == "Player2Char" ? 
 			new Vector2 (Input.GetAxisRaw ("Joystick X"), Input.GetAxisRaw ("Joystick Y")) : 
 			new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
 
-
 		md = Vector2.Scale (md, new Vector2 (sensitivity * smoothing, sensitivity * smoothing));
 		smoothV.x = Mathf.Lerp (smoothV.x, md.x, 1f / smoothing);
 		smoothV.y = Mathf.Lerp (smoothV.y, md.y, 1f / smoothing);
 		lookVector += smoothV;
-
+		lookVector.y = getLockedY (lookVector.y);
 		transform.localRotation = Quaternion.AngleAxis (-lookVector.y, Vector3.right);
 		character.transform.localRotation = Quaternion.AngleAxis (lookVector.x, character.transform.up);
+	}
+
+	private float getLockedY(float yValue){
+		return Mathf.Clamp (yValue, -90, 90);
 	}
 }
