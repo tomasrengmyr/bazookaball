ab5066ca421e56e8d28e830be52c22818a74cd88
peder <peder.a.jonsson@gmail.com>
2017-05-07 23:02:40 +0200

    fix for remove components script not running

---
+++ Assets/Scripts/RemoveComponents.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class RemoveComponents : NetworkBehaviour {
+
+
+	Camera sceneCamera;
+
+	[SerializeField]
+	Behaviour[] componentsToDisable;
+
+
+	void start(){
+		print ("removecomponents start");
+		if (!isLocalPlayer) {
+			print ("is not local player");
+			for (int i = 0; i < componentsToDisable.Length; i++) {
+				componentsToDisable [i].enabled = false;
+			}
+		} else {
+			print ("is local player");
+			sceneCamera = GameObject.FindWithTag("SceneCamera").GetComponent<Camera>();
+			if(sceneCamera != null){
+				print ("found scenecamera");
+				sceneCamera.gameObject.SetActive(false);	
+			}
+		}
+	}
+
+	void onDisable(){
+		sceneCamera.gameObject.SetActive(true);
+	}
+}
