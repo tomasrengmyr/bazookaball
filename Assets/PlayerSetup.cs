4f338f5d2b627f3d6f2eed2fd7e984e22d8010a3
peder <peder.a.jonsson@gmail.com>
2017-05-07 22:07:49 +0200

    spawn points and setup player script

--- Assets/PlayerSetup.cs
+++ Assets/PlayerSetup.cs
@@ -7,13 +7,22 @@ public class PlayerSetup : NetworkBehaviour {
 
 	[SerializeField]
 	Behaviour[] componentsToDisable;
+	Camera sceneCamera;
 
 	void start(){
-		if(!isLocalPlayer){
-			for(int i = 0; i < componentsToDisable.Length; i++){
+		if (!isLocalPlayer) {
+			for (int i = 0; i < componentsToDisable.Length; i++) {
 				componentsToDisable [i].enabled = false;
+			}
+		} else {
+			sceneCamera = GameObject.FindWithTag("SceneCamera").GetComponent<Camera>();
+			if(sceneCamera != null){
+				sceneCamera.gameObject.SetActive(false);	
 			}
 		}
 	}
 
+	void onDisable(){
+		sceneCamera.gameObject.SetActive(true);
+	}
 }
