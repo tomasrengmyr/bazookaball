4efd8286ae6d096763b07982c242d7cfcbfc012b
peder <peder.a.jonsson@gmail.com>
2017-05-07 21:25:35 +0200

    cleaning hierarchy

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	[SerializeField]
+	Behaviour[] componentsToDisable;
+
+	void start(){
+		if(!isLocalPlayer){
+			for(int i = 0; i < componentsToDisable.Length; i++){
+				componentsToDisable [i].enabled = false;
+			}
+		}
+	}
+
+}
